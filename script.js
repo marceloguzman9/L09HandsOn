89108670f799ae6c143c3f2fb043c13b92b379f8
Unknown <marcelo.guzman9@yahoo.com>
2018-06-02 01:11:53 -0700

    created 3 files, in einstein.json wrote code for bio, name, url

---
+++ script.js
@@ -0,0 +1,15 @@
+let newRequest = new XMLHttpRequest();
+newRequest.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    let myObj = JSON.parse(this.responseText);
+    document.getElementById("example").innerHTML = myObj.name;
+  }
+};
+newRequest.open("GET", "new.json", true);
+newRequest.send();
+
+
+
+//Create a function that will show the name, picture and birthday of Einstein when the page is loaded using the einstein.json file.
+//Create a button that will call the above function and will display his bio when clicked.
+//Don't forget the necessary HTML code to allow this all to work!
